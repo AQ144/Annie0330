e8130cf0d8282feda85862fa6d66241713faa8bd
Annie <jeffary8110@foxmail.com>
2016-05-05 19:34:13 +0800

    init project

--- src/test/java/an/xq/contorller/CotrollerTest.java
+++ src/test/java/an/xq/contorller/CotrollerTest.java
@@ -1,7 +1,14 @@
 package an.xq.contorller;
 
+import org.junit.Before;
+
 /**
  * Created by xiongqing on 2016-05-05.
  */
 public class CotrollerTest {
+
+    @Before
+    public void setup(){}
+
+
 }
