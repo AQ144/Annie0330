017c876fe6677bd64956790ec773ff696be705d8
jefferyxq <jeffary8110@hotmail.com>
2016-05-05 18:17:52 +0800

    init project

--- src/main/java/an/xq/Application.java
+++ src/main/java/an/xq/Application.java
@@ -1,7 +1,24 @@
 package an.xq;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
 /**
  * Created by xiongqing on 2016-05-05.
  */
+
+@SpringBootApplication
 public class Application {
+
+    private static ConfigurableApplicationContext appCtx;
+
+    public static void main(String[] args){
+        System.out.println("hello");
+        appCtx = SpringApplication.run(Application.class, args);
+
+    }
+
 }
--- src/main/java/an/xq/SimpleConfig.java
+++ src/main/java/an/xq/SimpleConfig.java
@@ -1,7 +1,10 @@
 package an.xq;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by xiongqing on 2016-05-05.
  */
+@Configuration
 public class SimpleConfig {
 }
